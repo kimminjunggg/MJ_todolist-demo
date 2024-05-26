2f023d65d1dd76828f58adc8b7ed04f7be907867
kimminjunggg <kmj030597@naver.com>
2024-05-27 00:18:45 +0900

    four commit

--- main.js
+++ main.js
@@ -88,17 +88,17 @@ function render() {
     if (list[i].isComplete == true) {
       resultHTML += `<div class="task">
       <div class= "task-done">${list[i].taskContent}</div>
-      <div>
-        <button onClick = "toggleComplete('${list[i].id}')">Check</button>
-        <button onClick = "deleteTask('${list[i].id}')">Delete</button>
+      <div class="chc-del-btn">
+        <button onClick = "toggleComplete('${list[i].id}')"><svg width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></button>
+        <button onClick = "deleteTask('${list[i].id}')"><svg width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button>
       </div>
    </div>`;
     } else {
       resultHTML += `<div class="task">
     <div>${list[i].taskContent}</div>
-    <div>
-      <button onClick = "toggleComplete('${list[i].id}')">Check</button>
-      <button onClick = "deleteTask('${list[i].id}')">Delete</button>
+    <div class="chc-del-btn">
+      <button onClick = "toggleComplete('${list[i].id}')"><svg width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></button>
+      <button onClick = "deleteTask('${list[i].id}')"><svg width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button>
     </div>
  </div>`;
     }
